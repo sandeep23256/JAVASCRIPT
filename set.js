25215352fdebb2de7251d89fddfaba20a3562566
sandeep23256 <sandeepmpct23@gmail.com>
2024-12-13 19:09:12 +0530

    first commit

---
+++ set.js
@@ -0,0 +1,17 @@
+data=document.getElementById("a").innerHTML =
+"<center><h2>Pninfosys Gwalior 1234</h2></center>";
+
+data=document.getElementById("a").setAttribute("style","border:10px dotted red");
+
+data=document.getElementById("a").removeAttribute("style");
+
+data=document.getElementById("a").removeAttribute("style");
+
+data=document.getElementById("b").innerHTML="<h1>Gwalior</h1>";
+
+//querySelector
+document.querySelector('#a').innerHTML=
+"<center><h2>Pninfosys Gwalior 1234</h2></center>";
+
+document.querySelector('.c').innerHTML=
+"<center><h2>Pninfosys Gwalior 12345566</h2></center>";
